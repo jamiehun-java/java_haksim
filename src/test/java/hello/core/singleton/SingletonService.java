ffd8b14a4df108083fdb0ed93d24dae5ba27a52f
jamiehun <jamiesunghun@gmail.com>
2023-03-16 23:32:11 +0900

    feat:chap06

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonService {
+    //1. static 영역에 객체를 딱 1개만 생성해둠
+    private static final SingletonService instance = new SingletonService();
+
+    //2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    //3. 생성자를 private으로 선언해서 외부에서 New 키워드로 사용한 객체 생성을 못하게 막음
+    private SingletonService(){
+    }
+
+    public void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+}
